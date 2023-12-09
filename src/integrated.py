db502ce4c9df2c5fbbeca06ca2d3d077bbbfcdd5
freorozcoloa <freorozcoloa@gmail.com>
2023-12-08 22:37:57 -0500

    [ADD] Integration test

--- src/integrated.py
+++ src/integrated.py
@@ -1,59 +1,95 @@
 from typing import Optional, Union
 from pathlib import Path
 import pandas as pd
-
+from .utils import create_report
 datasets = Path(__file__).resolve().parent.parent / "datasets"
 
-def read_lead():
+def read_datasets_leads()->pd.DataFrame:
     """Reads the dataset from a given path.
 
     Returns:
         pandas.DataFrame: The dataset.
     """
-    lead_path = datasets / "raw" / "leads.csv"
-    lead = pd.read_csv(lead_path)
-    return lead
+    path = datasets / "raw" / "leads.csv"
+    leads = pd.read_csv(path)
+    leads = leads.dropna(subset=["Id"])
+    return leads
 
-def get_ids(df:pd.DataFrame)->pd.DataFrame:
-    """Gets the ids of the dataset.
-
-    Args:
-        df (pandas.DataFrame): The dataset.
-
-    Returns:
-        pandas.DataFrame: The dataset with the ids.
-    """
-    serie_id = df["Id"].dropna()
-    uniques_id = serie_id.unique()
-    df_id = pd.DataFrame(uniques_id, columns=["Id"])
-    return df_id
-
-def save_values(df:pd.DataFrame, path: Optional[Path] = None)->pd.DataFrame:
-    """Saves the ids of the dataset.
+def preprocesing_city(df)->pd.DataFrame:
+    """Preprocesses the city column.
 
     Args:
         df (pandas.DataFrame): The dataset.
 
     Returns:
-        pandas.DataFrame: The dataset with the ids.
+        pandas.DataFrame: The dataset with preprocessed city column.
     """
-    if path is None:
-        path = datasets / "processed" / "ids.csv"
-    df.to_csv(path, index=False)
-    
-def preprocessing(df:pd.DataFrame, path_output: Optional[Path] = None)->pd.DataFrame:
-    """Preprocesses the dataset.
-
-    Args:
-        df (pandas.DataFrame): The dataset.
-
-    Returns:
-        pandas.DataFrame: The preprocessed dataset.
-    """
-    df = get_ids(df)
-    save_values(df, path_output)
+    df["has_city"] = ~df["City"].isnull()
+    df.drop(columns=["City"], inplace=True)
     return df
 
-if __name__ == "__main__":
-    df = read_lead()
-    preprocessing(df)
+def preprocessing_acquisition_campaign(df):
+    """Preprocesses the Acquisition campaign column.
+
+    Args:
+        df (pandas.DataFrame): The dataset.
+
+    Returns:
+        pandas.DataFrame: The dataset with preprocessed Acquisition campaign column.
+    """
+    major_values = ["VirtualMeetups", "EducationExpo", "TradeShow"]
+    df["acquisition_campaign"] = df["Acquisition campaign"].apply(lambda x: x if x in major_values else "Other")
+    df.drop(columns=["Acquisition campaign"], inplace=True)
+    return df
+
+def preprocesing_use_case(df)->pd.DataFrame:
+    """Preprocesses the Use case column.
+
+    Args:
+        df (pandas.DataFrame): The dataset.
+
+    Returns:
+        pandas.DataFrame: The dataset with preprocessed Use case column.
+    """
+    major_values = ["Corporate ", "eventes"]
+    df["use_case"] = df["Use case"].apply(lambda x: x if x in major_values else "Other")
+    df.drop(columns=["Use case"], inplace=True)
+    return df
+
+def preprocessing_source(df:pd.DataFrame)->pd.DataFrame:
+    """Preprocesses the Source column.
+
+    Args:
+        df (pandas.DataFrame): The dataset.
+
+    Returns:
+        pandas.DataFrame: The dataset with preprocessed Source column.
+    """
+    df["source"] = df["Source"]
+    df.drop(columns=["Source"], inplace=True)
+    return df
+
+def preprocessing_created_date(df:pd.DataFrame)->pd.DataFrame:
+    """Preprocesses the Created date column.
+
+    Args:
+        df (pandas.DataFrame): The dataset.
+
+    Returns:
+        pandas.DataFrame: The dataset with preprocessed Created date column.
+    """
+    df["created_date"] = pd.to_datetime(df["Created date"])
+    df.drop(columns=["Created date"], inplace=True)
+    return df
+
+def run():
+    """Preprocesses the dataset and saves it in the processed folder.
+    """
+    df = read_datasets_leads()
+    df = preprocesing_city(df)
+    df = preprocessing_acquisition_campaign(df)
+    df = preprocesing_use_case(df)
+    df = preprocessing_source(df)
+    df = preprocessing_created_date(df)
+    create_report(df, "leads")
+    df.to_csv(datasets / "processed" / "leads.csv", index=False)
---
+++ test/test_integrated.py
@@ -0,0 +1,106 @@
+import pytest
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from src.integrated import (
+    read_datasets_leads,
+    preprocesing_city,
+    preprocessing_acquisition_campaign,
+    preprocesing_use_case,
+    preprocessing_source,
+    preprocessing_created_date,
+)
+
+# Directorio de prueba
+test_datasets = Path("test_datasets")
+
+def test_read_datasets_leads():
+    # Llamar a la función read_datasets_leads
+    leads_df = read_datasets_leads()
+
+    # Verificar que el resultado es un DataFrame y no es nulo
+    assert isinstance(leads_df, pd.DataFrame)
+    assert not leads_df.empty
+
+    # Verificar que no hay valores nulos en la columna 'Id'
+    assert not leads_df['Id'].isnull().any()
+
+def test_preprocesing_city():
+    # Crear un DataFrame de prueba con la columna 'City'
+    df = pd.DataFrame({
+        'City': ['New York', 'Chicago', np.nan]
+    })
+
+    # Llamar a la función preprocesing_city
+    result_df = preprocesing_city(df)
+
+    # Verificar que el resultado es un DataFrame y no es nulo
+    assert isinstance(result_df, pd.DataFrame)
+    assert not result_df.empty
+
+    # Verificar que la columna 'has_city' se ha creado correctamente
+    assert 'has_city' in result_df.columns
+
+def test_preprocessing_acquisition_campaign():
+    # Crear un DataFrame de prueba con la columna 'Acquisition campaign'
+    df = pd.DataFrame({
+        'Acquisition campaign': ['VirtualMeetups', 'EducationExpo', 'Other']
+    })
+
+    # Llamar a la función preprocessing_acquisition_campaign
+    result_df = preprocessing_acquisition_campaign(df)
+
+    # Verificar que el resultado es un DataFrame y no es nulo
+    assert isinstance(result_df, pd.DataFrame)
+    assert not result_df.empty
+
+    # Verificar que la columna 'acquisition_campaign' se ha creado correctamente
+    assert 'acquisition_campaign' in result_df.columns
+
+def test_preprocesing_use_case():
+    # Crear un DataFrame de prueba con la columna 'Use case'
+    df = pd.DataFrame({
+        'Use case': ['Corporate ', 'eventes', 'Other']
+    })
+
+    # Llamar a la función preprocesing_use_case
+    result_df = preprocesing_use_case(df)
+
+    # Verificar que el resultado es un DataFrame y no es nulo
+    assert isinstance(result_df, pd.DataFrame)
+    assert not result_df.empty
+
+    # Verificar que la columna 'use_case' se ha creado correctamente
+    assert 'use_case' in result_df.columns
+
+def test_preprocessing_source():
+    # Crear un DataFrame de prueba con la columna 'Source'
+    df = pd.DataFrame({
+        'Source': ['Google', 'Facebook', 'Other']
+    })
+
+    # Llamar a la función preprocessing_source
+    result_df = preprocessing_source(df)
+
+    # Verificar que el resultado es un DataFrame y no es nulo
+    assert isinstance(result_df, pd.DataFrame)
+    assert not result_df.empty
+
+    # Verificar que la columna 'source' se ha creado correctamente
+    assert 'source' in result_df.columns
+
+def test_preprocessing_created_date():
+    # Crear un DataFrame de prueba con la columna 'Created date'
+    df = pd.DataFrame({
+        'Created date': ['2022-01-01', '2022-02-01']
+    })
+
+    # Llamar a la función preprocessing_created_date
+    result_df = preprocessing_created_date(df)
+
+    # Verificar que el resultado es un DataFrame y no es nulo
+    assert isinstance(result_df, pd.DataFrame)
+    assert not result_df.empty
+
+    # Verificar que la columna 'created_date' se ha creado correctamente
+    assert 'created_date' in result_df.columns
